f9faa410de1bb2981af6063f4f6a4ecc12edfca1
rockythechao <mack151.rockythechao@yahoo.com>
2015-09-13 09:55:20 -0600

    textblox - alignment and other properties working
    Needs heavy optimization.

--- LuaScriptsLib/textblox.lua
+++ LuaScriptsLib/textblox.lua
@@ -1,7 +1,7 @@
 --***************************************************************************************
 --                                                                                      *
 -- 	textblox.lua																		*
---  v0.1.0a                                                      						*
+--  v0.2.0a                                                      						*
 --  Documentation: ___											  						*
 --                                                                                      *
 --***************************************************************************************
@@ -46,29 +46,42 @@ do
 	end	
 
 	
-	function Font:drawCharImage (character, x,y)
+	function Font:drawCharImage (character, x,y, opacity)
 		
+		local alpha = opacity or 1.00
 		local index = string.byte(character,1)-33
 		local w = self.charWidth
 		local h = self.charHeight
 		local sourceX = (index%16) * w
 		local sourceY = math.floor(index/16) * h
 				
-		Graphics.drawImage (self.imageRef, x, y, sourceX, sourceY, w, h)
+		Graphics.drawImage (self.imageRef, x, y, sourceX, sourceY, w, h, alpha)
 	end
 	
 
-	function textblox.print (text, x,y, font, wrapWidth)
+	function textblox.print (text, x,y, font, halign, valign, width, opacity)
 		-- Setup
 		local lineBreaks = 0
 		local charsOnLine = 0
 		local strLength = text:len()
-		local wrapW = wrapWidth or 9999
+		
+		local lineStr = ""
+		
+		local totalWidth = 1
+		local totalHeight = 1
+		
+		local alpha = opacity or 1.00
+		
+		local startOfLine = 1
 		local currentLineWidth = 0
+		local fullLineWidth = 0
 		local charEndWidth = 0
 		local markupCount = 0
 		local i = 1
 		local totalShownChars = 0
+		
+		local t_halign = halign or textblox.HALIGN_LEFT
+		local t_valign = valign or textblox.VALIGN_TOP
 		
 		local lastSpaceX = nil
 		local lastSpaceY = nil
@@ -81,20 +94,85 @@ do
 		
 		while (i <= strLength) do 
 		
-			-- Get character
+			-- Get character info
 			local lastNum = math.max(1, i-1)
 			
 			local lastChar = text:sub(lastNum, lastNum)
 			local thisChar = text:sub(i,i)
 			local nextChar = text:sub(i+1,i+1)
+			
+			local endOfLineA = text:len() - startOfLine
+			local endOfLineB = text:len() - i
+			local endOfLineC = text:find ("<br", startOfLine)
+			local endOfLine = nil
+			
+			endOfLine = endOfLineC
+			
+			
+			
+			local throwawayVar = nil
+			local allLineBreaks = nil
+			throwawayVar, allLineBreaks  = text:gsub('<br>','<br>')
+			
+			if  allLineBreaks == nil  then
+				allLineBreaks = 1
+			end
+			
+			
+			
+			local lineStrA = text:sub (startOfLine, endOfLine)
+			local lineStrB = lineStrA:gsub ('<.->', '')
+			lineStrB = lineStrB:gsub ('<.*','')
+			lineStrB = lineStrB:gsub ('.->','')
+			
+			if  i == strLength  then
+				--windowDebug ("CURRENT LINE BREAKS: " .. lineBreaks .. ", ALL LINE BREAKS: " .. allLineBreaks)
+				--windowDebug ("START: " .. tostring(startOfLine) .. "\nEND A: " .. tostring(endOfLineA) .. "\nEND B: " .. tostring(endOfLineB)  .. "\nDECIDED END: " .. tostring(endOfLine) .. "\n\n" .. lineStrA .. "\n\n" .. lineStrB)
+			end
+			
 			local continue = false
 			
 			
+			
+			-- Get line width info
+			fullLineWidth = (lineStrB:len()) * (font.charWidth + font.kerning)
 			currentLineWidth = (charsOnLine) * (font.charWidth + font.kerning)
 			charEndWidth = (charsOnLine+1) * (font.charWidth + font.kerning)
+	
+			if 	fullLineWidth > totalWidth  then
+				totalWidth = fullLineWidth
+			end
 			
-			local xPos = x + currentLineWidth
-			local yPos = y + (lineBreaks*font.charHeight)
+			totalHeight = (lineBreaks+1)*font.charHeight
+			
+			
+			-- Determine the position based on alignment
+			local xPos = nil
+			local yPos = nil
+			local fullLine = text:match('.*<br>', i)
+
+			
+			if		t_halign == textblox.HALIGN_LEFT  then
+				xPos = x + currentLineWidth
+			
+			elseif	t_halign == textblox.HALIGN_RIGHT  then
+				xPos = x - fullLineWidth + currentLineWidth -- - font.charWidth
+			
+			else
+				xPos = x - 0.5*(fullLineWidth) + currentLineWidth
+			end
+			
+			
+			if		t_valign == textblox.VALIGN_TOP  then
+				yPos = y + (lineBreaks*font.charHeight)
+			
+			elseif	t_valign == textblox.VALIGN_BOTTOM  then
+				yPos = y + (lineBreaks - allLineBreaks - 1)*font.charHeight
+			
+			else
+				yPos = y + (lineBreaks*font.charHeight)	- ((allLineBreaks+1)*font.charHeight*0.5)
+			end
+			
 			
 			
 			-- Process inline commands
@@ -105,6 +183,8 @@ do
 				
 				-- Line break
 				if  text:sub (i, i+2) == '<br'  then 
+					startOfLine = endOfLine + 3
+					
 					lineBreaks = lineBreaks + 1
 					charsOnLine = 0
 					--i = i + 2
@@ -147,7 +227,7 @@ do
 				end
 
 				-- Ignore spaces
-				if  thisChar ~= ' '  then
+				if  thisChar ~= ' '  and  alpha > 0.0  then
 					
 					-- Process visual effects
 					local xAffected = xPos
@@ -167,7 +247,7 @@ do
 					
 					
 					-- Finally, draw the image
-					font:drawCharImage (thisChar, xAffected, yAffected)
+					font:drawCharImage (thisChar, xAffected, yAffected, alpha)
 				
 				
 				-- For debug purposes
@@ -185,14 +265,8 @@ do
 			i = i+1
 		end
 		
-		
-		-- Debug drawing
-		--if  lastSpaceX ~= nil  then
-			--graphX.boxScreen (lastSpaceX,lastSpaceY, font.charWidth,font.charHeight, 0x00990099)
-		--end
-		
-		
-		--windowDebug (text)
+		--windowDebug ("W: " .. tostring(totalWidth) .. ",  H: " .. tostring(totalHeight))
+		return totalWidth, totalHeight
 	end
 
 end
@@ -206,24 +280,24 @@ end
 --***************************************************************************************************
 
 do
-	textblox.BOXTYPE_NONE = 0
-	textblox.BOXTYPE_MENU = 1
-	textblox.BOXTYPE_WORDBUBBLE = 2
-	textblox.BOXTYPE_CUSTOM = 3
+	textblox.BOXTYPE_NONE = 1
+	textblox.BOXTYPE_MENU = 2
+	textblox.BOXTYPE_WORDBUBBLE = 3
+	textblox.BOXTYPE_CUSTOM = 4
 	
-	textblox.BIND_SCREEN = 0
-	textblox.BIND_LEVEL = 1
+	textblox.BIND_SCREEN = 1
+	textblox.BIND_LEVEL = 2
 
-	textblox.SCALE_FIXED = 0
-	textblox.SCALE_AUTO = 1
+	textblox.SCALE_FIXED = 1
+	textblox.SCALE_AUTO = 2
 	
-	textblox.HALIGN_LEFT = 0
-	textblox.HALIGN_MID = 1
-	textblox.HALIGN_RIGHT = 2
+	textblox.HALIGN_LEFT = 1
+	textblox.HALIGN_MID = 2
+	textblox.HALIGN_RIGHT = 3
 	
-	textblox.VALIGN_TOP = 0
-	textblox.VALIGN_MID = 1
-	textblox.VALIGN_BOTTOM = 2
+	textblox.VALIGN_TOP = 1
+	textblox.VALIGN_MID = 2
+	textblox.VALIGN_BOTTOM = 3
 	
 	
 	TextBlock = {}
@@ -243,26 +317,42 @@ do
 		thisTextBlock.boxType = properties["boxType"] or textblox.BOXTYPE_MENU
 		thisTextBlock.boxTex = properties["boxTex"]
 		thisTextBlock.boxColor = properties["boxColor"] or 0x00AA0099 			-- transparent green
+		
 		thisTextBlock.scaleMode = properties["scaleMode"] or textblox.SCALE_FIXED
-		thisTextBlock.width =  = properties["w"] or 200
-		thisTextBlock.height =  = properties["h"] or 200
+		thisTextBlock.width = properties["width"] or 200
+		thisTextBlock.height = properties["height"] or 200
 		
 		thisTextBlock.bind = properties["bind"] or textblox.BIND_SCREEN
-		thisTextBlock.halign = properties["halign"] or textblox.HALIGN_LEFT
-		thisTextBlock.valign = properties["valign"] or textblox.VALIGN_TOP
+		
+		thisTextBlock.halign = properties["textAnchorX"] or textblox.HALIGN_LEFT
+		thisTextBlock.valign = properties["textAnchorY"] or textblox.VALIGN_TOP
+		
+		thisTextBlock.textAlpha = properties["textAlpha"] or 1
+		
+		thisTextBlock.boxhalign = properties["boxAnchorX"] or textblox.HALIGN_LEFT
+		thisTextBlock.boxvalign = properties["boxAnchorY"] or textblox.VALIGN_TOP
 				
 		thisTextBlock.font = properties["font"] or textblox.FONT_DEFAULT
 		
 		thisTextBlock.speed = properties["speed"] or 0.5
 		
-		thisTextBlock.xMargin = properties["xmargin"] or 0.0
-		thisTextBlock.yMargin = properties["ymargin"] or 0.0
+		thisTextBlock.xMargin = properties["marginX"] or 4
+		thisTextBlock.yMargin = properties["marginY"] or 4
 
+		thisTextBlock.visible = properties["visible"]
+		if  thisTextBlock.visible == nil  then
+			thisTextBlock.visible = true
+		end
+		
 		
 		-- Control vars
 		thisTextBlock.pauseFrames = 0
 		thisTextBlock.shakeFrames = 0
 
+		thisTextBlock.autoWidth = 1
+		thisTextBlock.autoHeight = 1
+		
+		
 		thisTextBlock.filteredText = textStr
 		thisTextBlock.length = string.len(textStr)
 		
@@ -296,38 +386,126 @@ do
 	
 	
 	function TextBlock:draw ()
+		-- Get width and height
+		local textToShow = string.sub(self:getTextWrapped (), 1, self.charsShown)
+		
+		
+		--self.autoWidth = 4
+		--self.autoHeight = 4
+		---[[
+		self.autoWidth, self.autoHeight = textblox.print   (textToShow, 
+															9999, 
+															9999,
+															self.font,
+															self.halign,
+															self.valign,
+															self.width,
+															0.0)
+		--]]
+		
+	
 		-- Get shake offset
 		local shakeX = math.random (-12, 12) * (self.shakeFrames/8)
 		local shakeY = math.random (-12, 12) * (self.shakeFrames/8)		
 		
 		
-		-- Draw box
-		textblox.drawMenuBox   (self.x - self.xMargin + shakeX,  
-								self.y - self.yMargin + shakeY,
-								self.width + 2*self.xMargin, 
-								self.height + 2*self.yMargin,
-								self.boxColor)
-	
-	
-		-- Draw debug stuff
+		-- Get alignment and width based on scale mode
+		local boxAlignX = self.boxhalign
+		local boxAlignY = self.boxvalign
+		local boxWidth = self.width
+		local boxHeight = self.height 
+		
+		if  self.scaleMode == textblox.SCALE_AUTO  then
+			boxAlignX = self.halign
+			boxAlignY = self.valign
+			boxWidth = self.autoWidth
+			boxHeight = self.autoHeight 
+			--windowDebug ("TEST\n\n" .. tostring(self.autoWidth) .. "," .. tostring(self.autoHeight))
+		end
+
 		--[[
-		graphX.boxScreen   (self.x + (self:getCharsPerLine ()) * (self.font.charWidth + self.font.kerning),
-							self.y, 
-							10,
-							self.height, 
-							0x00990099)
+		if  self.autoWidth == nil  then
+			boxWidth = 4
+		end
+		if  self.autoHeight == nil  then
+			boxHeight = 4
+		end
 		--]]
 		
-	
-		-- Display text
-		local textToShow = string.sub(self:getTextWrapped (), 1, self.charsShown)
-		--local wrappedText = textToShow
+		-- Get box positioning based on anchors
+		local boxX = nil
+		local boxY = nil
 		
-		textblox.print (textToShow, 
-						self.x + shakeX, 
-						self.y + shakeY,
-						self.font,
-						self.width)
+		if		boxAlignX == textblox.HALIGN_LEFT  then
+			boxX = self.x + shakeX
+		
+		elseif	boxAlignX == textblox.HALIGN_RIGHT  then
+			boxX = self.x - boxWidth + shakeX
+		
+		else
+			boxX = self.x - (0.5*boxWidth) + shakeX
+		end
+
+		
+		if		boxAlignY == textblox.VALIGN_TOP  then
+			boxY = self.y + shakeY
+		
+		elseif	boxAlignY == textblox.VALIGN_BOTTOM  then
+			boxY = self.y - boxHeight + shakeY
+		
+		else
+			boxY = self.y - (0.5*boxHeight) + shakeY
+		end
+
+		
+		
+		-- Draw box
+		if  self.boxType == textblox.BOXTYPE_MENU  then
+			textblox.drawMenuBox   (boxX-self.xMargin,  
+									boxY-self.yMargin,
+									boxWidth + 2*self.xMargin, 
+									boxHeight + 2*self.yMargin,
+									self.boxColor)
+		end
+		
+
+		-- Get text positioning based on anchors
+		local textX = nil
+		local textY = nil
+		
+		---[[
+		if		self.halign == textblox.HALIGN_LEFT  then
+			textX = boxX
+		
+		elseif	self.halign == textblox.HALIGN_RIGHT  then
+			textX = boxX + boxWidth
+		
+		else
+			textX = boxX + 0.5*boxWidth
+		end
+
+		
+		if		self.valign == textblox.VALIGN_TOP  then
+			textY = boxY
+		
+		elseif	self.valign == textblox.VALIGN_BOTTOM  then
+			textY = boxY + boxHeight
+		
+		else
+			textY = boxY + 0.5*boxHeight
+		end
+		--]]
+		
+
+		-- Display text
+		self.autoWidth, self.autoHeight = textblox.print   (textToShow, 
+															textX, 
+															textY,
+															self.font,
+															self.halign,
+															self.valign,
+															self.width,
+															self.textAlpha)
 	end
 
 
@@ -347,6 +525,10 @@ do
 	
 	function TextBlock:update ()
 		self:updateTiming ()
+		
+		if  self.visible == true  then
+			self.autoWidth,self.autoHeight = self:draw ()
+		end
 	end
 	
 	
@@ -453,7 +635,7 @@ do
 					-- ...then add the length of the command to the displayed characters to skip the command
 					if  abortNow == true  then
 						continueSkipping = false
-						self.charsShown = self.charsShown
+						self.charsShown = self.charsShown - 1
 					end
 					
 					self.charsShown = self.charsShown + fullCommand:len()
